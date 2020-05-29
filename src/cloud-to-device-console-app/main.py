f0924be1f8d7ddb7c1927f36941cdb7bbc28521c
luisgizirian <luisg@australnova.com>
2020-05-29 15:48:26 -0300

    C# to Python port of C2D console complete.

--- src/cloud-to-device-console-app/main.py
+++ src/cloud-to-device-console-app/main.py
@@ -1,19 +1,10 @@
-import sys
-import linecache
 import json
 from os import path
-import argparse
-import logging
 import pathlib
-
+import logging
 from builtins import input
-
 import ssl
 import urllib.request
-
-# Must install following packages
-#   pip install azure-iot-device
-#   pip install azure-iot-hub
 from azure.iot.hub import IoTHubRegistryManager
 from azure.iot.hub.models import CloudToDeviceMethod, CloudToDeviceMethodResult
 
@@ -21,41 +12,94 @@ def urlToText(url):
     url = url.replace(path.sep, '/')
     resp = urllib.request.urlopen(url, context=ssl._create_unverified_context())
     return resp.read()
-    
+
 class GraphManager:
-    def __init__(self, ioThubConnectionString, deviceId, moduleId, operationsApiVersion):
-        self._iotHubConnStr = ioThubConnectionString
-        self._deviceId = deviceId
-        self._moduleId = moduleId
-        self._apiVersion = operationsApiVersion
+    
+    def __init__(self):
+        with open('appsettings.json', 'r') as settings_file:
+            config_data = settings_file.read()
 
-        self._registryManager = IoTHubRegistryManager(self._iotHubConnStr)
+        config = json.loads(config_data)
+        
+        with open('operations.json') as ops_file:
+            ops_data = ops_file.read()
 
-    def InvokeModuleMethod(self, methodName, payload):
-        moduleMethod = CloudToDeviceMethod(method_name=methodName, payload=payload)
-        return self._registryManager.invoke_device_module_method(self._deviceId, self._moduleId, moduleMethod)
+        self.ops = json.loads(ops_data)
 
-    def GraphTopologySet(self, opParams):
-        if opParams is None:
+        self.device_id = str(config['deviceId'])
+        self.module_id = str(config['moduleId'])
+        self.generic_methods = [
+            'GraphTopologyList',
+            'GraphInstanceList',
+            'GraphInstanceActivate',
+            'GraphInstanceDeactivate',
+            'GraphInstanceDelete',
+            'GraphInstanceGet',
+            'GraphInstanceSet',
+            'GraphTopologyDelete',
+            'GraphTopologyGet']
+        self.api_version = '1.0'
+
+        self.registry_manager = IoTHubRegistryManager(str(config['IoThubConnectionString']))
+
+        for i,j in enumerate(self.ops['operations'], start=1):
+            
+            self.invocation_proxy(j['opName'], j['opParams'])
+
+    def invocation_proxy(self, method_name, payload):
+        if method_name=='GraphTopologySet':
+            self.graph_topology_set(payload)
+        
+        if method_name in self.generic_methods:
+            self.generic_call(method_name, payload)
+
+        if method_name=='WaitForInput':
+            print(payload['message'])
+            print('Press <ENTER> to continue...')
+            input()
+
+    
+    def invoke_module_method(self, method_name, payload):
+        moduleMethod = CloudToDeviceMethod(
+            method_name=method_name,
+            payload=payload,
+            response_timeout_in_seconds=30)
+
+        print("\n-----------------------  Request: % s  --------------------------------------------------\n"% method_name)
+        print(payload)
+        
+        resp = self.registry_manager.invoke_device_module_method(self.device_id, self.module_id, moduleMethod)
+        
+        print("\n---------------  Response: % s - Status: % s  ---------------\n"%(method_name, resp.status))
+
+        if resp.payload is not None:
+            print(resp.payload)
+
+    def graph_topology_set(self, op_parameters):
+        if op_parameters is None:
             logging.info('Operation parameters missing')
             raise Exception
 
-        if opParams.get('topologyUrl') is not None:
-            topologyJsonString = urlToText(opParams['topologyUrl'])
-        elif opParams.get('topologyFile') is not None:
-            fpath = 'file://' + path.join(pathlib.Path(__file__).parent.absolute(), opParams['topologyFile'])
+        if op_parameters.get('topologyUrl') is not None:
+            topologyJsonString = urlToText(op_parameters['topologyUrl'])
+        elif op_parameters.get('topologyFile') is not None:
+            fpath = 'file://' + path.join(pathlib.Path(__file__).parent.absolute(), op_parameters['topologyFile'])
             topologyJsonString = urlToText(fpath)
         else:
             logging.info('Neither topologyUrl nor topologyFile specified')
 
         topologyJson = json.loads(topologyJsonString)
 
-        return self.InvokeModuleMethod('GraphTopologySet', topologyJson)
+        return self.invoke_module_method('graph_topology_set', topologyJson)
 
-    def GenericCall(self, methodName, opParams):
-        if opParams is None:
+    def generic_call(self, method_name, op_parameters):
+        if op_parameters is None:
             logging.info('Operation parameters missing')
             raise Exception
         
-        opParams['@apiVersion'] = self._apiVersion
-        return self.InvokeModuleMethod(methodName, opParams)
+        op_parameters['@apiVersion'] = self.api_version
+        return self.invoke_module_method(method_name, op_parameters)
+
+
+if __name__ == '__main__':
+    manager = GraphManager()
