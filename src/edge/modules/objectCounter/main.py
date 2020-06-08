93828ac2e928e56cd65db18e4ae8db6473a2a081
luisgizirian <luisg@australnova.com>
2020-06-08 21:28:54 +0000

    objectCounter Module first port proposal.

--- src/edge/modules/objectCounter/main.py
+++ src/edge/modules/objectCounter/main.py
@@ -3,105 +3,129 @@
 # full license information.
 
 import time
-import os
 import sys
 import asyncio
 import json
 from six.moves import input
-import threading
 from azure.iot.device.aio import IoTHubModuleClient
 from azure.iot.device import Message
 
-class ModuleObjectCounter:
-
-    def __init__(self):
-        self.object_tag = ''
-        self.object_confidence = 0
+OBJECT_TAG = 'truck'
+OBJECT_CONFIDENCE = 0.5
+TWIN_CALLBACKS = 0
 
 
-    async def main(self):
-        try:
-            if not sys.version >= "3.5.3":
-                raise Exception( "The sample requires python 3.5.3+. Current version of Python: %s" % sys.version )
-            print ( "IoT Hub Client for Python" )
+async def main():
+    try:
+        # twin_patch_listener is invoked when the module twin's desired properties are updated.
+        async def twin_patch_listener(module_client):
+            global OBJECT_TAG
+            global OBJECT_CONFIDENCE
+            global TWIN_CALLBACKS
 
-            # The client object is used to interact with your Azure IoT hub.
-            module_client = IoTHubModuleClient.create_from_edge_environment()
+            while True:
+                try:
+                    data = await module_client.receive_twin_desired_properties_patch()  # blocking call
+                    print('The data in the desired properties patch was: %s' % data)
+                    if 'objectTag' in data:
+                        OBJECT_TAG = data["objectTag"]
+                    if 'objectConfidence' in data:
+                        OBJECT_CONFIDENCE = data['objectConfidence']
+                    TWIN_CALLBACKS += 1
+                    print('Total calls confirmed: %d\n' % TWIN_CALLBACKS)
+                except Exception as ex:
+                    print('Unexpected error in twin_patch_listener: %s' % ex)
 
-            # connect the client.
-            await module_client.connect()
+        async def count_objects_listener(module_client):
+            global OBJECT_TAG
+            global OBJECT_CONFIDENCE
 
-            # define behavior for receiving an input message on input1
-            async def count_objects_listener(module_client):
-                while True:
-                    input_message = await module_client.receive_message_on_input("detectedObjects")  # blocking call
-                    
+            while True:
+                input_message = await module_client.receive_message_on_input("detectedObjects")  # blocking call    
+
+                if input_message is not None:
+                    message = input_message.data
+                    size = len(message)
+                    message_text = message.decode('utf-8')
                     count = 0
-                    detected_objects = input_message.custom_properties['inferences']
 
-                    if detected_objects is not None:
+                    #
+                    data = json.loads(message_text)
+                    #
+
+                    try:
+                        detected_objects = data['inferences']
                         for inference in detected_objects:
                             entity = inference['entity']
                             tag = entity['tag']
 
-                            if (tag["value"] == self.object_tag) and (tag["confidence"] > self.object_confidence):
+                            if (tag["value"] == OBJECT_TAG) and (tag["confidence"] > OBJECT_CONFIDENCE):
                                 count += 1
-                    
-                    if count > 0:
-                        output_message_string = json.dumps(dict({'count': count}))
 
-                    output_message = Message(output_message_string)
-                    
-                    subject = input_message.custom_properties['subject']
-                    graph_instance_signature = '/graphInstances/'
+                        if count > 0:
+                            output_message_string = json.dumps(dict({'count': count}))
 
-                    if graph_instance_signature in subject:
-                        # CSharp sample version is doing nothing with this, so does this Python port (must it?)
-                        graph_instance_name = subject.split('/')[2]
-                        #
-                        output_message.custom_properties['eventTime'] = input_message.custom_properties['eventTime']
-                        await module_client.send_message_to_output(output_message, "objectCountTrigger")
+                            output_message = Message(output_message_string, content_encoding='utf-8')
+                            
+                            subject = input_message.custom_properties['subject']
+                            graph_instance_signature = '/graphInstances/'
 
-            
-            # define behavior for halting the application
-            def stdin_listener(self):
-                while True:
-                    try:
-                        selection = input("Press Q to quit\n")
-                        if selection == "Q" or selection == "q":
-                            print("Quitting...")
-                            break
+                            if graph_instance_signature in subject:
+                                # CSharp sample version is doing nothing with this, so does this Python port (must it?)
+                                graph_instance_name = subject.split('/')[2]
+                                #output_message.custom_properties['eventTarget'] = graph_instance_name
+                                #
+                                output_message.custom_properties['eventTime'] = input_message.custom_properties['eventTime']
+                                await module_client.send_message_to_output(output_message, "objectCountTrigger")
                     except:
-                        time.sleep(10)
+                        print('No inferences array found for message.')
 
+        def stdin_listener():
+            while True:
+                try:
+                    selection = input("Press Q to quit\n")
+                    if selection == "Q" or selection == "q":
+                        print("Quitting...")
+                        break
+                except:
+                    time.sleep(10)
 
-            # Schedule task for C2D Listener
-            listeners = asyncio.gather(count_objects_listener(module_client))
+        if not sys.version >= "3.5.3":
+            raise Exception("The sample requires python 3.5.3+. Current version of Python: %s" % sys.version)
+        print("IoT Hub Client for Python")
 
-            print ( "The sample is now waiting for messages. ")
+        # The client object is used to interact with your Azure IoT hub.
+        module_client = IoTHubModuleClient.create_from_edge_environment()
 
-            # Run the stdin listener in the event loop
-            loop = asyncio.get_event_loop()
-            user_finished = loop.run_in_executor(None, stdin_listener)
+        # connect the client.
+        await module_client.connect()
 
-            # Wait for user to indicate they are done listening for messages
-            await user_finished
+        # Schedule task for C2D Listener1
+        listeners = asyncio.gather(count_objects_listener(module_client), twin_patch_listener(module_client))
 
-            # Cancel listening
-            listeners.cancel()
+        print("The sample is now waiting for messages. ")
 
-            # Finally, disconnect
-            await module_client.disconnect()
+        # Run the stdin listener in the event loop
+        loop = asyncio.get_event_loop()
+        user_finished = loop.run_in_executor(None, stdin_listener)
 
-        except Exception as e:
-            print ( "Unexpected error %s " % e )
-            raise
+        # Wait for user to indicate they are done listening for messages
+        await user_finished
+
+        # Cancel listening
+        listeners.cancel()
+
+        # Finally, disconnect
+        await module_client.disconnect()
+
+    except Exception as e:
+        print("Unexpected error %s " % e)
+        raise
 
 if __name__ == "__main__":
-    module = ModuleObjectCounter()
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(module.main())
+    loop.run_until_complete(main())
     loop.close()
 
     # If using Python 3.7 or above, you can use following code instead:
